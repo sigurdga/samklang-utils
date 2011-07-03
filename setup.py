9308edcada84eb9592a925bf98b7b18bf9c10120
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-07-03 02:53:38 +0200

    Add middleware package

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from distutils.core import setup
 setup(
         name = 's7n-utils',
         version = "1a1",
-        packages = ['s7n', 's7n.utils'],
+        packages = ['s7n', 's7n.utils', 's7n.utils.middleware'],
         )
