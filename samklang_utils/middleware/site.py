5a9837e7b7cf1f234cd40cd72e880baea66376b2
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-12-05 13:58:51 +0100

    Port is ignored. Check is working.

--- samklang_utils/middleware/site.py
+++ samklang_utils/middleware/site.py
@@ -2,8 +2,8 @@
 class LazySite(object):
     def __get__(self, request, obj_type=None):
         if not hasattr(request, '_cached_site'):
-            from site.models import Site
-            request._cached_site = Site.objects.get(request.get_host().lower())
+            from django.contrib.sites.models import Site
+            request._cached_site = Site.objects.get(domain=request.get_host().split(":")[0].lower())
         return request._cached_site
 
 class SiteMiddleware(object):
