20155027fa6f595a9f1228103a6c4f4a40479594
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-12-12 00:15:09 +0100

    Update version number to 0.3.1 to get in the missing manager.

--- samklang_utils/forms.py
+++ samklang_utils/forms.py
@@ -13,3 +13,10 @@ class MarkdownTextarea(forms.Textarea):
         self.attrs['class'] += " flext growme maxheight-500"
         return super(MarkdownTextarea, self).get_context_data()
 
+class SharedToGroup(forms.MultiWidget):
+    #template_name = 'samklang_utils/shared_to_group.html'
+
+    def __init__(self, attrs=None):
+        widgets = (forms.Checkbox, forms.Modelselect)
+        super(SharedToGroup, self).__init__(widgets, attrs)
+
